3c0b439715f810b5cb0d3f6bb82ba8937eebdafb
PRAVEN2004 <dpraveenkumar231@gmail.com>
2025-03-02 13:43:42 +0530

    frontend completed

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -24,7 +24,8 @@ const PlaceOrder = () => {
 <input type="text" placeholder='Phone'/>
       </div>
       <div className="place-order-right">
-      <h2>Cart Totals</h2>
+      <div className="cart-total">
+            <h2>Cart Totals</h2>
             <div>
               <div className="cart-total-details">
                 <p>Subtotal</p>
@@ -33,15 +34,16 @@ const PlaceOrder = () => {
               <hr/>
               <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>${2}</p>
+              <p>${getTotalCartAmount()===0?0:2}</p>
               </div>
               <hr/>
               <div className="cart-total-details">
                 <b>Total</b>
-                <b>${getTotalCartAmount()+2}</b>
+                <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
               </div>
              </div>
-             <button onClick={()=>navigate('/order')}>PROCEED TO PAYMENT</button>
+             <button >PROCEED TO PAYMENT</button>
+          </div>
           </div>
       
     </form>
